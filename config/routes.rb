895f08f488c7aa4f6cc37974ad9b06a850d3bf58
Ilya Andreyuk <ilya.andreyuk@warecorp.com>
2015-07-20 19:48:44 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RedmineApp::Application.routes.draw do
   get 'bulk_time_entries' => 'bulk_time_entries#index'
-  get 'load_assigned_issues' => 'bulk_time_entries#load_assigned_issues'
+  post 'load_assigned_issues' => 'bulk_time_entries#load_assigned_issues'
   post 'save' => 'bulk_time_entries#save'
   post 'add_entry' => 'bulk_time_entries#add_entry'
 
